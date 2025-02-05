1634044514f1c1d466224d8f15288f0970954199
Voltron <barrettschake@Barretts-MacBook-Pro.local>
2025-02-05 08:55:22 -0700

    added voltron.py with new brainstorming notes

---
+++ voltron.py
@@ -0,0 +1,9 @@
+"""
+Psuedocode/brainstorming:
+- give user an intro blurb
+- take current pw
+- measure:
+    - length
+    - charset
+    - check against breach database..? maybe shortened version of rockyou, top 100k or such?
+"""
