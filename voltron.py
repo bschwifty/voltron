c7be3f4011f21fccdc6e223a34e81cabb8786952
Voltron <barrettschake@Barretts-MacBook-Pro.local>
2025-02-07 21:28:11 -0700

    added a spot for help function

--- voltron.py
+++ voltron.py
@@ -13,3 +13,6 @@ print("Welcome to Voltron, the password analyzer.  How strong is your password?"
 print("Please enter your password.  We won't save it, but if you are justifiably paranoid,")
 password = input(" you can put in a similar one for us to analyze instead.")
 print(password)
+
+def help:
+    pass
