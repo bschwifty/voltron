ec6900a3f227cb86d80d6b474412ac4c333e6f20
Voltron <barrettschake@Barretts-MacBook-Pro.local>
2025-02-07 21:23:41 -0700

    intro and first couple lines

--- voltron.py
+++ voltron.py
@@ -7,3 +7,7 @@ Psuedocode/brainstorming:
     - charset
     - check against breach database..? maybe shortened version of rockyou, top 100k or such?
 """
+
+
+print("Welcome to Voltron, the password analyzer.  How strong is your password?")
+input("Please enter your password.  We won't save it, but if you are justifiably paranoid, you can put in a similar one for us to analyze instead.")
