8f143c6421dc880d6b13842d0838d367f4ad13d7
Voltron <barrettschake@Barretts-MacBook-Pro.local>
2025-02-07 21:24:51 -0700

    reformatted; debug print stmt

--- voltron.py
+++ voltron.py
@@ -10,4 +10,6 @@ Psuedocode/brainstorming:
 
 
 print("Welcome to Voltron, the password analyzer.  How strong is your password?")
-input("Please enter your password.  We won't save it, but if you are justifiably paranoid, you can put in a similar one for us to analyze instead.")
+print("Please enter your password.  We won't save it, but if you are justifiably paranoid,")
+password = input(" you can put in a similar one for us to analyze instead.")
+print(password)
