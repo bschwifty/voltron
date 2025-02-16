6af39695441a9e59dd8397f4c3ecea2c30c26afe
Voltron <barrettschake@Barretts-MacBook-Pro.local>
2025-02-16 15:16:17 -0700

    added shell of analysis function

--- voltron.py
+++ voltron.py
@@ -13,11 +13,14 @@ Psuedocode/brainstorming:
 
 print("Welcome to Voltron, the password helper-outer.  How strong is your password?")
 
-proceed = input("Would you like to check your password now (Y/N)?\n")
+proceed = input("Would you like to check your password now (Y/N)?\n").strip().upper()
 
 if proceed == "N":
     print("No problem, have a nice day.")
     exit()
+
+if proceed == "Y":
+    analysis()
 
 print("Please enter your password.  We won't save it, but if you are justifiably paranoid,")
 password = input(" you can put in a similar one for us to analyze instead.\n")
@@ -26,3 +29,7 @@ print(password)
 def help():
     print("Voltron is a command-line utility to analyze the strength of your password.")
     print("Simply type your password in, and Voltron will tell you whether it's a good one or not")
+
+def analysis():
+    print("TODO: analysis goes here")
+    pass
