be78efef81bde4ec399587e47393d4a73b9a44eb
Voltron <barrettschake@Barretts-MacBook-Pro.local>
2025-02-16 15:18:12 -0700

    rearrangement

--- voltron.py
+++ voltron.py
@@ -10,6 +10,13 @@ Psuedocode/brainstorming:
     - check against breach database..? maybe shortened version of rockyou, top 100k or such?
 """
 
+def help():
+    print("Voltron is a command-line utility to analyze the strength of your password.")
+    print("Simply type your password in, and Voltron will tell you whether it's a good one or not")
+
+def analysis():
+    print("TODO: analysis goes here")
+    pass
 
 print("Welcome to Voltron, the password helper-outer.  How strong is your password?")
 
@@ -25,11 +32,3 @@ if proceed == "Y":
 print("Please enter your password.  We won't save it, but if you are justifiably paranoid,")
 password = input(" you can put in a similar one for us to analyze instead.\n")
 print(password)
-
-def help():
-    print("Voltron is a command-line utility to analyze the strength of your password.")
-    print("Simply type your password in, and Voltron will tell you whether it's a good one or not")
-
-def analysis():
-    print("TODO: analysis goes here")
-    pass
