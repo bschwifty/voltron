92ef132d10b29661fc7368dc5894066a2b6657a4
Voltron <barrettschake@Barretts-MacBook-Pro.local>
2025-02-28 22:06:15 -0700

    added handling for correcthorsebatterystaple

--- voltron.py
+++ voltron.py
@@ -17,6 +17,10 @@ def analysis():
     print("Please enter your password.  We won't save it, but if you are justifiably paranoid,")
     password = pwinput.pwinput(prompt=' you can put in a similar one for us to analyze instead.\n')
     print(password) # debug only, remove this after testing
+    # XKCD check:
+    if password == "correcthorsebatterystaple":
+        print("That's an excellent password, but everyone else has read XKCD too...")
+    # Length checks:
     if len(password) < 16:
         print("Your password is shorter than 16 characters long, consider making it longer.")
     elif len(password) >= 16 and len(password) < 25:
