ec3730aabf74304bf719e15af1d4e43e0ae7497e
Voltron <barrettschake@Barretts-MacBook-Pro.local>
2025-07-21 10:39:37 +0100

    added another print stmt to end of char score section

--- voltron.py
+++ voltron.py
@@ -44,6 +44,8 @@ def analysis_charset():
     print(f"Your password uses {char_score} character types.")
     if char_score < 4:
         print("Consider adding more character types to your password.\n")
+    else:
+        print("Nice job!\n")
     # TODO add print stmts here to effect of "your pw uses x character types"
 
 '''
